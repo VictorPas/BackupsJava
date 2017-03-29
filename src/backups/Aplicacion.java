32fd88d400d35de37f69443ce36451160b6346ad
victor <pokevik21@gmail.com>
2017-03-29 17:53:46 +0200

    Primer commit

---
+++ src/backups/Aplicacion.java
@@ -0,0 +1,18 @@
+
+package backups;
+
+
+public class Aplicacion {
+
+    public static void main(String[] args) throws Exception {
+        Backups b = new Backups();
+        b.hacerBackup();
+        
+        /**
+         * Configurar el archivo Configurar.txt, para que funcione correctamente,
+         * Espero que te guste mi aplicacion.
+         */
+            
+    }
+
+}
